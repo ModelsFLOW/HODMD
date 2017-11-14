627d637e23d27bd1fe68a07926ae18322a90b9cf
LeClainche <31996161+LeClainche@users.noreply.github.com>
2017-11-14 17:45:20 +0100

    Update DMD1_SIADS.m

--- DMD1_SIADS.m
+++ DMD1_SIADS.m
@@ -1,7 +1,7 @@
 
  % This file is part of HODMD
  %
- % Copyright (c) 2017 LeClainche
+ % Copyright (c) 2017 S Le Clainche & J M Vega
  % All rights reserved.
  %
  % Redistribution and use in source and binary forms, with or without
